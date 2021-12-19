5ce7eca5e798d58f46e0617603f1c26f5609cf52
trapano.monogamo <filippo.cozzi.fc@gmail.com>
2021-12-19 17:09:48 +0100

    abstracted Game class

--- Launcher/Launcher.cs
+++ Launcher/Launcher.cs
@@ -1,35 +1,222 @@
-﻿using OpenTK.Mathematics;
+﻿using OpenTK.Graphics.OpenGL4;
+using OpenTK.Windowing.Common;
 using OpenTK.Windowing.Desktop;
+using OpenTK.Windowing.GraphicsLibraryFramework;
+using OpenTK.Mathematics;
+using System.Collections.Generic;
+using System.Linq;
+using renderable;
+using shader;
+using texture;
 using game;
+
+// TODO : refactor naming of everything
+// --- classes:
+// TODO : Material class
+// TODO : Mesh class
+// TODO : ControlHelper/InputHelper helper class
+// TODO : LightSource
+//          -> consider making a lightSources list in the game (in the future scene class) and render them first, then the renderableObjects
+// DONE : Scene (renderables + light sources + camera)
+// DONE : abstract the game class (use the default OnLoad etc, and call there the OnLoad defined by the user their derived class)
+// --- features:
+// TODO : integrate .obj and .mtl files with the renderable creation process
 
 namespace physics_goes_brr
 {
-    public class MyGame : Game {
-        public MyGame(GameWindowSettings gameWindowSettings, NativeWindowSettings nativeWindowSettings)
-            : base(gameWindowSettings, nativeWindowSettings)
-        {}
+   public class MyGame : Game
+   {
+      public MyGame(GameWindowSettings gameWindowSettings, NativeWindowSettings nativeWindowSettings)
+          : base(gameWindowSettings, nativeWindowSettings)
+      { }
 
-        protected override void UserLoad() {}
+      protected override void UserLoad()
+      {
+         GL.ClearColor(0.2f, 0.2f, 0.2f, 1.0f);
+         GL.Enable(EnableCap.DepthTest);
 
-        protected override void UserUnload() {}
+         // -- resource loading code --
 
-        protected override void UserInput() {}
+         //var temp = ObjectLoader.LoadObjFile("./res/models/untitled.obj");
+         //temp.Print();
 
-        protected override void UserRender() {}
+         scene.camera.Move(new Vector3(.0f, .0f, 3.0f));
 
-        protected override void UserUpdate() {}
-    }
+         // normal cube with texture
+         scene.renderableObjects.Add(new Renderable(
+            new float[]{
+            // position            color                     texture coords
+            -0.5f, -0.5f,  0.5f,   1.0f, 0.0f, 0.0f, 1.0f,   0.0f, 0.0f,   // near top    right
+             0.5f, -0.5f,  0.5f,   1.0f, 0.0f, 0.0f, 1.0f,   1.0f, 0.0f,   // near bottom right
+             0.5f,  0.5f,  0.5f,   1.0f, 0.0f, 0.0f, 1.0f,   1.0f, 1.0f,   // near bottom left
+            -0.5f,  0.5f,  0.5f,   1.0f, 0.0f, 0.0f, 1.0f,   0.0f, 1.0f,   // near top    left
+            -0.5f, -0.5f, -0.5f,   1.0f, 0.0f, 0.0f, 1.0f,   0.0f, 0.0f,   // far  top    right
+             0.5f, -0.5f, -0.5f,   1.0f, 0.0f, 0.0f, 1.0f,   1.0f, 0.0f,   // far  bottom right
+             0.5f,  0.5f, -0.5f,   1.0f, 0.0f, 0.0f, 1.0f,   1.0f, 1.0f,   // far  bottom left
+            -0.5f,  0.5f, -0.5f,   1.0f, 0.0f, 0.0f, 1.0f,   0.0f, 1.0f    // far  top    left
+            },
+            new uint[]{
+               // front face
+               0, 1, 2,
+               2, 3, 0,
+               // back face
+               7, 6, 5,
+               5, 4, 7,
+               // left face
+               4, 5, 1,
+               1, 0, 4,
+               // right face
+               3, 2, 6,
+               6, 7, 3,
+               // top face
+               1, 5, 6,
+               6, 2, 1,
+               // bottom face
+               4, 0, 3,
+               3, 7, 4,
+            },
+            new Shader("./res/shaders/vert_shader.vert", "./res/shaders/frag_shader.frag"),
+            new Texture[] { new Texture("./res/textures/container.jpg"), new Texture("./res/textures/awesomeface.jpg")/*, new Texture("./res/trollface.jpg")*/ },
+            "cube"
+         ));
 
-    class Launcher
-    {
-        static void Main(string[] args)
-        {
-            GameWindowSettings gameWindowSettings = GameWindowSettings.Default;
-            NativeWindowSettings nativeWindowSettings = NativeWindowSettings.Default;
-            nativeWindowSettings.Title = "Hello OpenTK";
-            nativeWindowSettings.Size = new Vector2i(840,840);
-            MyGame myApp = new MyGame(gameWindowSettings, nativeWindowSettings);
-            myApp.Start();
-        }
-    }
+         // light source
+         scene.renderableObjects.Add(new Renderable(
+            new float[]{
+            // position            color                     texture coords
+            -0.5f, -0.5f,  0.5f,   1.0f, 1.0f, 1.0f, 1.0f,   0.0f, 0.0f,   // near top    right
+             0.5f, -0.5f,  0.5f,   1.0f, 1.0f, 1.0f, 1.0f,   1.0f, 0.0f,   // near bottom right
+             0.5f,  0.5f,  0.5f,   1.0f, 1.0f, 1.0f, 1.0f,   1.0f, 1.0f,   // near bottom left
+            -0.5f,  0.5f,  0.5f,   1.0f, 1.0f, 1.0f, 1.0f,   0.0f, 1.0f,   // near top    left
+            -0.5f, -0.5f, -0.5f,   1.0f, 1.0f, 1.0f, 1.0f,   0.0f, 0.0f,   // far  top    right
+             0.5f, -0.5f, -0.5f,   1.0f, 1.0f, 1.0f, 1.0f,   1.0f, 0.0f,   // far  bottom right
+             0.5f,  0.5f, -0.5f,   1.0f, 1.0f, 1.0f, 1.0f,   1.0f, 1.0f,   // far  bottom left
+            -0.5f,  0.5f, -0.5f,   1.0f, 1.0f, 1.0f, 1.0f,   0.0f, 1.0f    // far  top    left
+            },
+            new uint[]{
+               // front face
+               0, 1, 2,
+               2, 3, 0,
+               // back face
+               7, 6, 5,
+               5, 4, 7,
+               // left face
+               4, 5, 1,
+               1, 0, 4,
+               // right face
+               3, 2, 6,
+               6, 7, 3,
+               // top face
+               1, 5, 6,
+               6, 2, 1,
+               // bottom face
+               4, 0, 3,
+               3, 7, 4,
+            },
+            new Shader("./res/shaders/vert_shader.vert", "./res/shaders/frag_shader.frag"),
+            new Texture[] { /*new Texture("./res/textures/container.jpg"), new Texture("./res/textures/awesomeface.jpg")*/ },
+            "lamp"
+         ));
+
+         var lamp = scene.renderableObjects.FindLast(x => x.name == "lamp");
+         //this.lightSource = new LightSource(LightSourceType.Point, new Color4(1.0f, 1.0f, 1.0f, 1.0f), ref lamp);;
+         lamp.Translate(new Vector3(2.5f, 1.3f, -1.2f));
+         lamp.Scale(new Vector3(0.5f, 0.5f, 0.5f));
+         //System.Console.WriteLine(System.String.Format("lamp: {0}, {1} \t ls: {2}, {3}", lamp.position.X, lamp.position.Y, lightSource.position.X, lightSource.position.Y));
+      }
+
+      protected override void UserUnload()
+      {
+         scene.Delete();
+      }
+
+      protected override void UserInput(FrameEventArgs args)
+      {
+         var keyboard = KeyboardState;
+
+         if (keyboard.IsKeyDown(Keys.Escape))
+         {
+            base.shouldClose = true;
+         }
+         if (keyboard.IsKeyPressed(Keys.M))
+         {
+            base.wireframeMode = !wireframeMode;
+         }
+         if (keyboard.IsKeyPressed(Keys.N))
+         {
+            base.mouseFocused = !base.mouseFocused;
+            CursorGrabbed = !CursorGrabbed;
+            if (!CursorGrabbed) { CursorVisible = true; }
+         }
+
+         Vector3 horizontalDir = Vector3.Normalize(new Vector3(scene.camera.dir.X, 0.0f, scene.camera.dir.Z));
+         Vector3 horizontalUp = new Vector3(0.0f, 1.0f, 0.0f);
+
+         if (keyboard.IsKeyDown(Keys.W))
+         {
+            scene.camera.Move(horizontalDir * speed * (float)args.Time);
+         }
+         if (keyboard.IsKeyDown(Keys.A))
+         {
+            scene.camera.Move((-scene.camera.right) * speed * (float)args.Time);
+         }
+         if (keyboard.IsKeyDown(Keys.S))
+         {
+            scene.camera.Move((-horizontalDir) * speed * (float)args.Time);
+         }
+         if (keyboard.IsKeyDown(Keys.D))
+         {
+            scene.camera.Move(scene.camera.right * speed * (float)args.Time);
+         }
+         if (keyboard.IsKeyDown(Keys.Space))
+         {
+            scene.camera.Move(horizontalUp * speed * (float)args.Time);
+         }
+         if (keyboard.IsKeyDown(Keys.LeftShift))
+         {
+            scene.camera.Move((-horizontalUp) * speed * (float)args.Time);
+         }
+
+         var mouse = MouseState;
+
+         if (mouseFocused)
+         {
+            float dx = mouse.X - lastPos.X;
+            float dy = mouse.Y - lastPos.Y;
+            scene.camera.pitch -= dy * sensitivity;
+            scene.camera.yaw += dx * sensitivity;
+            lastPos = new Vector2(mouse.X, mouse.Y);
+            //System.Console.WriteLine(System.String.Format("x: {0}, y: {1}\t dx: {2}, dy: {3}", mouse.X, mouse.Y, dx, dy));
+            scene.camera.UpdateDirection();
+         }
+      }
+
+      protected override void UserUpdate(FrameEventArgs args)
+      {
+         scene.renderableObjects[0].Rotate(new Vector3(.0f, .1f, .2f));
+      }
+
+      protected override void UserRender(FrameEventArgs args)
+      {
+         GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+
+         // -- RENDERING CODE --
+         scene.Render(wireframeMode);
+
+         Context.SwapBuffers();
+      }
+   }
+
+   class Launcher
+   {
+      static void Main(string[] args)
+      {
+         GameWindowSettings gameWindowSettings = GameWindowSettings.Default;
+         NativeWindowSettings nativeWindowSettings = NativeWindowSettings.Default;
+         nativeWindowSettings.Title = "Hello OpenTK";
+         nativeWindowSettings.Size = new Vector2i(840, 840);
+         MyGame myApp = new MyGame(gameWindowSettings, nativeWindowSettings);
+         myApp.Start();
+      }
+   }
 }
--- app/Game.cs
+++ app/Game.cs
@@ -1,38 +1,22 @@
-using System.Collections.Generic;
-using System.Linq;
 using OpenTK.Graphics.OpenGL4;
 using OpenTK.Windowing.Common;
 using OpenTK.Windowing.Desktop;
-using OpenTK.Windowing.GraphicsLibraryFramework;
 using OpenTK.Mathematics;
-using renderable;
-using shader;
-using texture;
 using scene;
-
-// TODO : refactor naming of everything
-// TODO : put renderable mesh data into mesh class
-// TODO : MouseControl/MouseHelper helper class
-// TODO : LightSource
-//          -> consider making a lightSources list in the game (in the future scene class) and render them first, then the renderableObjects
-// TODO : material
-// DONE : Scene (renderables + light sources + camera)
-// TODO : abstract the game class (use the default OnLoad etc, and call there the OnLoad defined by the user their derived class)
-// TODO : integrate .obj and .mtl files with the renderable creation process
 
 namespace game
 {
    public class Game : GameWindow
    {
-      bool wireframeMode;
-      float speed;
-      float sensitivity;
-      Vector2 lastPos;
-      bool mouseFocused;
-      bool shouldClose = false;
-      Vector2 windowSize;
+      protected bool wireframeMode;
+      protected float speed;
+      protected float sensitivity;
+      protected Vector2 lastPos;
+      protected bool mouseFocused;
+      protected bool shouldClose = false;
+      protected Vector2 windowSize;
 
-      public Scene scene;
+      protected Scene scene;
 
       public Game(GameWindowSettings gameWindowSettings, NativeWindowSettings nativeWindowSettings)
          : base(gameWindowSettings, nativeWindowSettings)
@@ -53,118 +37,20 @@ namespace game
 
       protected override void OnLoad()
       {
-         GL.ClearColor(0.2f, 0.2f, 0.2f, 1.0f);
-         GL.Enable(EnableCap.DepthTest);
-
-         // -- resource loading code --
-
-         //var temp = ObjectLoader.LoadObjFile("./res/models/untitled.obj");
-         //temp.Print();
-
-         scene.camera.Move(new Vector3(.0f, .0f, 3.0f));
-         
-         // normal cube with texture
-         scene.renderableObjects.Add(new Renderable(
-            new float[]{
-            // position            color                     texture coords
-            -0.5f, -0.5f,  0.5f,   1.0f, 0.0f, 0.0f, 1.0f,   0.0f, 0.0f,   // near top    right
-             0.5f, -0.5f,  0.5f,   1.0f, 0.0f, 0.0f, 1.0f,   1.0f, 0.0f,   // near bottom right
-             0.5f,  0.5f,  0.5f,   1.0f, 0.0f, 0.0f, 1.0f,   1.0f, 1.0f,   // near bottom left
-            -0.5f,  0.5f,  0.5f,   1.0f, 0.0f, 0.0f, 1.0f,   0.0f, 1.0f,   // near top    left
-            -0.5f, -0.5f, -0.5f,   1.0f, 0.0f, 0.0f, 1.0f,   0.0f, 0.0f,   // far  top    right
-             0.5f, -0.5f, -0.5f,   1.0f, 0.0f, 0.0f, 1.0f,   1.0f, 0.0f,   // far  bottom right
-             0.5f,  0.5f, -0.5f,   1.0f, 0.0f, 0.0f, 1.0f,   1.0f, 1.0f,   // far  bottom left
-            -0.5f,  0.5f, -0.5f,   1.0f, 0.0f, 0.0f, 1.0f,   0.0f, 1.0f    // far  top    left
-            },
-            new uint[]{
-               // front face
-               0, 1, 2, 
-               2, 3, 0,
-               // back face
-               7, 6, 5,
-               5, 4, 7,
-               // left face
-               4, 5, 1,
-               1, 0, 4,
-               // right face
-               3, 2, 6,
-               6, 7, 3,
-               // top face
-               1, 5, 6,
-               6, 2, 1,
-               // bottom face
-               4, 0, 3,
-               3, 7, 4,
-            },
-            new Shader("./res/shaders/vert_shader.vert", "./res/shaders/frag_shader.frag"),
-            new Texture[]{ new Texture("./res/textures/container.jpg"), new Texture("./res/textures/awesomeface.jpg")/*, new Texture("./res/trollface.jpg")*/ },
-            "cube"
-         ));
-         
-         // light source
-         scene.renderableObjects.Add(new Renderable(
-            new float[]{
-            // position            color                     texture coords
-            -0.5f, -0.5f,  0.5f,   1.0f, 1.0f, 1.0f, 1.0f,   0.0f, 0.0f,   // near top    right
-             0.5f, -0.5f,  0.5f,   1.0f, 1.0f, 1.0f, 1.0f,   1.0f, 0.0f,   // near bottom right
-             0.5f,  0.5f,  0.5f,   1.0f, 1.0f, 1.0f, 1.0f,   1.0f, 1.0f,   // near bottom left
-            -0.5f,  0.5f,  0.5f,   1.0f, 1.0f, 1.0f, 1.0f,   0.0f, 1.0f,   // near top    left
-            -0.5f, -0.5f, -0.5f,   1.0f, 1.0f, 1.0f, 1.0f,   0.0f, 0.0f,   // far  top    right
-             0.5f, -0.5f, -0.5f,   1.0f, 1.0f, 1.0f, 1.0f,   1.0f, 0.0f,   // far  bottom right
-             0.5f,  0.5f, -0.5f,   1.0f, 1.0f, 1.0f, 1.0f,   1.0f, 1.0f,   // far  bottom left
-            -0.5f,  0.5f, -0.5f,   1.0f, 1.0f, 1.0f, 1.0f,   0.0f, 1.0f    // far  top    left
-            },
-            new uint[]{
-               // front face
-               0, 1, 2, 
-               2, 3, 0,
-               // back face
-               7, 6, 5,
-               5, 4, 7,
-               // left face
-               4, 5, 1,
-               1, 0, 4,
-               // right face
-               3, 2, 6,
-               6, 7, 3,
-               // top face
-               1, 5, 6,
-               6, 2, 1,
-               // bottom face
-               4, 0, 3,
-               3, 7, 4,
-            },
-            new Shader("./res/shaders/vert_shader.vert", "./res/shaders/frag_shader.frag"),
-            new Texture[]{ /*new Texture("./res/textures/container.jpg"), new Texture("./res/textures/awesomeface.jpg")*/ },
-            "lamp"
-         ));
-
-         var lamp = scene.renderableObjects.FindLast(x => x.name == "lamp");
-         //this.lightSource = new LightSource(LightSourceType.Point, new Color4(1.0f, 1.0f, 1.0f, 1.0f), ref lamp);;
-         lamp.Translate(new Vector3(2.5f, 1.3f, -1.2f));
-         lamp.Scale(new Vector3(0.5f, 0.5f, 0.5f));
-         //System.Console.WriteLine(System.String.Format("lamp: {0}, {1} \t ls: {2}, {3}", lamp.position.X, lamp.position.Y, lightSource.position.X, lightSource.position.Y));
-
+         UserLoad();
          base.OnLoad();
       }
 
       protected override void OnUnload()
       {
-         scene.Delete();
+         UserUnload();
          base.OnUnload();
       }
 
       protected override void OnRenderFrame(FrameEventArgs args)
       {
-         GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
-
-         // -- RENDERING CODE --
-
-         scene.Render(wireframeMode);
-
-         Context.SwapBuffers();
+         UserRender(args);
          base.OnRenderFrame(args);
-
          // finish rendering before exiting
          if (this.shouldClose) { base.Close(); }
       }
@@ -175,61 +61,8 @@ namespace game
             return;
          }
 
-         var keyboard = KeyboardState;
-
-         if (keyboard.IsKeyDown(Keys.Escape))
-         {
-            this.shouldClose = true;
-         }
-         if (keyboard.IsKeyPressed(Keys.M))
-         {
-            this.wireframeMode = !wireframeMode;
-         }
-         if (keyboard.IsKeyPressed(Keys.N))
-         {
-            mouseFocused = !mouseFocused;
-            CursorGrabbed = !CursorGrabbed;
-            if (!CursorGrabbed) { CursorVisible = true; }
-         }
-
-         Vector3 horizontalDir = Vector3.Normalize(new Vector3(scene.camera.dir.X, 0.0f, scene.camera.dir.Z));
-         Vector3 horizontalUp = new Vector3(0.0f, 1.0f, 0.0f);
-
-         if (keyboard.IsKeyDown(Keys.W)) {
-            scene.camera.Move(horizontalDir * speed * (float)args.Time);
-         }
-         if (keyboard.IsKeyDown(Keys.A)) {
-            scene.camera.Move((-scene.camera.right) * speed * (float)args.Time);
-         }
-         if (keyboard.IsKeyDown(Keys.S)) {
-            scene.camera.Move((-horizontalDir) * speed * (float)args.Time);
-         }
-         if (keyboard.IsKeyDown(Keys.D)) {
-            scene.camera.Move(scene.camera.right * speed * (float)args.Time);
-         }
-         if (keyboard.IsKeyDown(Keys.Space)) {
-            scene.camera.Move(horizontalUp * speed * (float)args.Time);
-         }
-         if (keyboard.IsKeyDown(Keys.LeftShift)) {
-            scene.camera.Move((-horizontalUp) * speed * (float)args.Time);
-         }
-
-         var mouse = MouseState;
-
-         if (mouseFocused) {
-            float dx = mouse.X - lastPos.X;
-            float dy = mouse.Y - lastPos.Y;
-            scene.camera.pitch -= dy * sensitivity;
-            scene.camera.yaw += dx * sensitivity;
-            lastPos = new Vector2(mouse.X, mouse.Y);
-            //System.Console.WriteLine(System.String.Format("x: {0}, y: {1}\t dx: {2}, dy: {3}", mouse.X, mouse.Y, dx, dy));
-            scene.camera.UpdateDirection();
-         }
-
-         scene.renderableObjects[0].Rotate(new Vector3(.0f, .1f, .2f));
-
-         this.UserInput();
-         this.UserUpdate();
+         this.UserInput(args);
+         this.UserUpdate(args);
 
          base.OnUpdateFrame(args);
       }
@@ -248,8 +81,8 @@ namespace game
 
       protected virtual void UserLoad() {}
       protected virtual void UserUnload() {}
-      protected virtual void UserInput() {}
-      protected virtual void UserRender() {}
-      protected virtual void UserUpdate() {}
+      protected virtual void UserInput(FrameEventArgs args) {}
+      protected virtual void UserUpdate(FrameEventArgs args) {}
+      protected virtual void UserRender(FrameEventArgs args) {}
    }
 }
